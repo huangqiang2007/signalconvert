b6ed4bb01be7526929cd7f46ae4ab87315619e03
huangqiang <huangqiang@onespacechina.com>
2019-06-18 16:47:36 +0800

    remove some useless files

--- src/main.c
+++ src/main.c
@@ -1,27 +1,19 @@
+#include <string.h>
 #include "em_device.h"
 #include "em_chip.h"
 #include "em_cmu.h"
 #include "em_adc.h"
 #include "adcdrv.h"
 #include "dmactrl.h"
-//#include "temperature.h"
 #include "msg.h"
 #include "em_cmu.h"
-//#include "i2csisconfig.h"
-//#include "i2csis.h"
 #include "timer.h"
-#include "crc.h"
 #include "udelay.h"
 #include "hal-config.h"
 #include "main.h"
-#include "Uart.h"
-#include "EFM32_Timer.h"
-#include <string.h>
 #include "Typedefs.h"
-#define CMU_MODE_DIGEXTCLK    2
 
-uint32_t sleeptime=30;//30s»½ÐÑÒ»´Î
-int sleepflag=0;
+#define CMU_MODE_DIGEXTCLK	2
 
 void CMU_ClockModeSet(uint32_t mode)
 {
@@ -55,7 +47,7 @@ int main(void)
 	/*
 	 * Uart init for delivering converted data
 	 * */
-	Uart_Init();
+	//Uart_Init();
 
 	/*
 	 * config 7 ADC channels
