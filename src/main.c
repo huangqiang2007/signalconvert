8d83382220ac49d9a3071232cd36294db295095b
huangqiang <huangqiang@onespacechina.com>
2019-06-20 11:08:11 +0800

    reconfig system clock source

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@
 #include "em_adc.h"
 #include "adcdrv.h"
 #include "dmactrl.h"
-#include "msg.h"
 #include "em_cmu.h"
 #include "timer.h"
 #include "udelay.h"
@@ -14,26 +13,13 @@
 #include "uartdrv.h"
 #include "Typedefs.h"
 
-#define CMU_MODE_DIGEXTCLK	2
-
-void CMU_ClockModeSet(uint32_t mode)
-{
-  CMU->CTRL = (CMU->CTRL & ~_CMU_CTRL_HFXOMODE_MASK) | mode;
-}
-
 void Clock_config(void)
 {
-	/*
-	 * Enable clocks required
-	 * */
-	/* Prescale the HFCORECLK -> HF/2 = 50/2 = 25Mhz */
-	//CMU_ClockDivSet(cmuClock_HF, cmuClkDiv_2);
-	CMU_ClockEnable(cmuClock_HFPER, true);
-	CMU_ClockEnable(cmuClock_GPIO, true);
-	CMU_ClockEnable(cmuClock_USART0, true);
-
 	SystemCoreClockUpdate();
 
+	/*
+	 * chose external crystal oscillator as clock source.
+	 * */
 	CMU_OscillatorEnable(cmuOsc_HFXO, true, true);
 	CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFXO);
 	CMU_OscillatorEnable(cmuOsc_HFRCO, false, false);
@@ -41,6 +27,14 @@ void Clock_config(void)
 	CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_ULFRCO);
 	CMU_OscillatorEnable(cmuSelect_ULFRCO, true, true);
 	CMU_ClockEnable(cmuClock_HFLE, true);
+
+	/*
+	 * Enable clocks required
+	 * */
+	//CMU_ClockDivSet(cmuClock_HF, cmuClkDiv_2);
+	CMU_ClockEnable(cmuClock_HFPER, true);
+	CMU_ClockEnable(cmuClock_GPIO, true);
+	CMU_ClockEnable(cmuClock_USART0, true);
 }
 
 int main(void)
